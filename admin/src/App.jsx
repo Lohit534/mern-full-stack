0d2b24e062793f2710b4c28ae5ed86cf56af0d8f
Lohit534 <22h41a0534lohit@gmail.com>
2026-01-02 11:14:23 +0530

    Fix Admin routing: redirect root to Add page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
@@ -31,6 +31,7 @@ const App = () => {
           <div className="app-content">
             <Sidebar />
             <Routes>
+              <Route path="/" element={<Navigate to="/add" />} />
               <Route path="/add" element={<Add url={url} token={token} />} />
               <Route path="/list" element={<List url={url} token={token} />} />
               <Route path="/orders" element={<Orders url={url} token={token} />} />
