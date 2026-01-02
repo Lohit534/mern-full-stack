54d4c9e0a915e3e4bcf6b6fa5a308ead55a6b3f4
Lohit534 <22h41a0534lohit@gmail.com>
2026-01-02 15:15:42 +0530

    Isolate Portal page: Remove Navbar/Footer and app layout for root path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Home from './pages/Home/Home'
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Cart from './pages/Cart/Cart'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
@@ -15,24 +15,25 @@ import Verify from './pages/Verify/Verify'
 const App = () => {
 
   const [showLogin, setShowLogin] = useState(false);
+  const location = useLocation();
+  const isPortal = location.pathname === '/';
 
   return (
     <>
       <ToastContainer />
       {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
-      <div className='app'>
-        <Navbar setShowLogin={setShowLogin} />
+      <div className={isPortal ? '' : 'app'}>
+        {!isPortal && <Navbar setShowLogin={setShowLogin} />}
         <Routes>
           <Route path='/' element={<Portal />} />
           <Route path='/home' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<PlaceOrder />} />
           <Route path='/myorders' element={<MyOrders />} />
-          <Route path='/myorders' element={<MyOrders />} />
           <Route path='/verify' element={<Verify />} />
         </Routes>
       </div>
-      <Footer />
+      {!isPortal && <Footer />}
     </>
   )
 }
